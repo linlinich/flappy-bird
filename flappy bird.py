8ff9366b69d04478e3161fa9303bcadcbe5e8e79
Kate <45261018+katelk@users.noreply.github.com>
2019-04-25 16:36:04 +0300

    Update flappy bird.py

--- flappy bird.py
+++ flappy bird.py
@@ -6,3 +6,15 @@ import sys
 pygame.init()
 screen = pygame.display.set_mode([500, 600])
 screen.fill(pygame.Color('blue'))
+
+
+def load_image(name, colorkey):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    if colorkey != 0:
+        image.set_colorkey(colorkey)
+    return image
